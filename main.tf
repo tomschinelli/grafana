8d2d434827d044196a695a7f04da6e7d6476ce8d
Tom Schinelli <tom.schinelli@gmail.com>
2023-05-02 21:03:03 +0200

    Add cloudflare dns record

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 resource "hcloud_ssh_key" "default" {
   name       = "tomschinelli"
   public_key = file("~/.ssh/id_rsa.pub")
@@ -10,4 +9,15 @@ resource "hcloud_server" "grafana" {
   server_type = var.server_type
   location    = var.location
   ssh_keys    = [hcloud_ssh_key.default.id]
+}
+data "cloudflare_zone" "grafana" {
+  account_id = "657a56c8bdb8f013919004e139c98591"
+  name       = "tomschinelli.io"
+
+}
+resource "cloudflare_record" "grafana" {
+  name    = "grafana.tomschinelli.io"
+  value   = hcloud_server.grafana.ipv4_address
+  type    = "A"
+  zone_id = data.cloudflare_zone.grafana.id
 }
--- provider.tf
+++ provider.tf
@@ -9,22 +9,37 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = "eu-central-1"
 }
-data "aws_secretsmanager_secret_version" "hcloud_secret" {
+data "aws_secretsmanager_secret_version" "hcloud" {
   secret_id = "hcloud"
 }
 
+data "aws_secretsmanager_secret_version" "cloudflare" {
+  secret_id = "cloudflare"
+}
+
 locals {
-  hcloud_secret = jsondecode(
-    data.aws_secretsmanager_secret_version.hcloud_secret.secret_string
+  hcloud = jsondecode(
+    data.aws_secretsmanager_secret_version.hcloud.secret_string
+  )
+  cloudflare = jsondecode(
+    data.aws_secretsmanager_secret_version.cloudflare.secret_string
   )
 }
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token = local.hcloud_secret.token
+  token = local.hcloud.token
+}
+
+provider "cloudflare" {
+  api_token = local.cloudflare.token
 }
